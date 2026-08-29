14f4f9e55b32f67de817f2b3ca0cab95d8ca494a
carlon <carlon@google.com>
2026-08-28 23:31:40 -0400

    feat: Run integration tests for all supported configs.
    This involves changing the integration tests to use a unique bucket per
    test.

--- run_fio_integration_tests.sh
+++ run_fio_integration_tests.sh
@@ -21,6 +21,7 @@ TESTBENCH_HELPER_SH="$3"
 FIO_VALIDATOR_PY="$4"
 TESTBENCH_SETUP_PY="$5"
 TESTBENCH_RUN_PY="$6"
+COUNTER=1
 
 # shellcheck disable=SC1090
 source "${TESTBENCH_HELPER_SH?}"
@@ -32,14 +33,15 @@ run_fio_test() {
   echo "===================================================="
   echo "Running FIO test: ${test_name}..."
   echo "===================================================="
-  
+  local bucket="${test_name?}_${COUNTER?}"
+  : $((COUNTER = COUNTER + 1))
+
   # Create a bucket for this specific test
   curl -s -X POST "http://localhost:${HTTP_PORT}/storage/v1/b?project=test-project" \
     -H "Content-Type: application/json" \
-    -d "{\"name\": \"${test_name}\"}" > /dev/null
+    -d "{\"name\": \"${bucket?}\"}" > /dev/null
 
-  "${FIO_BINARY?}" --name="${test_name?}" "${FIO_COMMON[@]}" "$@" \
-    >"${TEST_TMPDIR?}/fio_out.json"
+  "${test_name?}" "${bucket?}" >"${TEST_TMPDIR?}/fio_out.json"
   
   # Validate output
   python3 "${FIO_VALIDATOR_PY}" --json-file "${TEST_TMPDIR?}/fio_out.json"
@@ -47,8 +49,136 @@ run_fio_test() {
   echo "Test ${test_name} PASSED and cleaned up."
 }
 
-# Callback function called by the testbench helper
-execute_tests() {
+single_stream_reads_buffered() {
+  local bucket="$1"
+  "${FIO_BINARY?}" --name=single_stream_reads_buffered "${FIO_COMMON[@]}" \
+    --rw=randread \
+    --bs=8K \
+    --filesize=4M \
+    --size=4M \
+    --numjobs=1 \
+    --nrfiles=1 \
+    --iodepth=1 \
+    --filename="${bucket?}"/file \
+    --direct=0
+}
+
+single_stream_reads_direct() {
+  local bucket="$1"
+  "${FIO_BINARY?}" --name=single_stream_reads_direct "${FIO_COMMON[@]}" \
+    --rw=randread \
+    --bs=8K \
+    --filesize=4M \
+    --size=4M \
+    --numjobs=1 \
+    --nrfiles=1 \
+    --iodepth=1 \
+    --filename="${bucket?}"/file \
+    --direct=1
+}
+
+single_stream_iodepth() {
+  local bucket="$1"
+  "${FIO_BINARY?}" --name=single_stream_iodepth "${FIO_COMMON[@]}" \
+    --rw=randread \
+    --bs=8K \
+    --filesize=8M \
+    --numjobs=1 \
+    --nrfiles=1 \
+    --iodepth=8 \
+    --filename="${bucket?}"/file
+}
+
+multi_job_shared_client() {
+  local bucket="$1"
+  "${FIO_BINARY?}" --name=multi_job_shared_client "${FIO_COMMON[@]}" \
+    --rw=randread \
+    --bs=8K \
+    --filesize=8M \
+    --numjobs=4 \
+    --nrfiles=1 \
+    --iodepth=1 \
+    --filename_format="${bucket?}"'/file.$jobnum' \
+    --go-storage-threads-share-client=1
+}
+
+multi_job_unique_clients() {
+  local bucket="$1"
+  "${FIO_BINARY?}" --name=multi_job_unique_clients "${FIO_COMMON[@]}" \
+    --rw=randread \
+    --bs=8K \
+    --filesize=8M \
+    --numjobs=4 \
+    --nrfiles=1 \
+    --iodepth=1 \
+    --filename_format="${bucket?}"'/file.$jobnum' \
+    --go-storage-threads-share-client=0
+}
+
+write_buffered() {
+  local bucket="$1"
+  "${FIO_BINARY?}" --name=write_buffered "${FIO_COMMON[@]}" \
+    --rw=write \
+    --bs=4K \
+    --filesize=8M \
+    --numjobs=1 \
+    --iodepth=1 \
+    --filename="${bucket?}"/file \
+    --direct=0
+}
+
+write_direct() {
+  local bucket="$1"
+  "${FIO_BINARY?}" --name=write_direct "${FIO_COMMON[@]}" \
+    --rw=write \
+    --bs=4K \
+    --filesize=8M \
+    --numjobs=1 \
+    --iodepth=1 \
+    --filename="${bucket?}"/file \
+    --direct=1
+}
+
+multi_write() {
+  local bucket="$1"
+  "${FIO_BINARY?}" --name=multi_write "${FIO_COMMON[@]}" \
+    --rw=write \
+    --bs=4K \
+    --filesize=2M \
+    --numjobs=4 \
+    --iodepth=1 \
+    --filename_format="${bucket?}"'/file.$jobnum'
+}
+
+single_job_multi_write() {
+  local bucket="$1"
+  "${FIO_BINARY?}" --name=single_job_multi_write "${FIO_COMMON[@]}" \
+    --rw=write \
+    --bs=4K \
+    --filesize=2M \
+    --numjobs=1 \
+    --nrfiles=4 \
+    --iodepth=1 \
+    --filename_format="${bucket?}"'/file.$filenum'
+}
+
+large_reads() {
+  local bucket="$1"
+  "${FIO_BINARY?}" --name=large_reads "${FIO_COMMON[@]}" \
+    --rw=randread \
+    --bs=1M \
+    --filesize=4M \
+    --numjobs=3 \
+    --nrfiles=1 \
+    --iodepth=2 \
+    --filename_format="${bucket?}"'/file.$jobnum'
+}
+
+execute_one_test() {
+  echo "###"
+  echo "# STARTING a fio test"
+  echo "# $*"
+  echo "###"
   local HTTP_PORT=$1
   local GRPC_PORT=$2
   local FIO_COMMON=(
@@ -60,100 +190,33 @@ execute_tests() {
     "--output-format=json"
   )
 
-  run_fio_test "single_stream_reads_buffered" \
-    --rw=randread \
-    --bs=8K \
-    --filesize=4M \
-    --numjobs=1 \
-    --nrfiles=1 \
-    --iodepth=1 \
-    --filename=single_stream_reads_buffered/file \
-    --direct=0
-
-  run_fio_test "single_stream_reads_direct" \
-    --rw=randread \
-    --bs=8K \
-    --filesize=4M \
-    --numjobs=1 \
-    --nrfiles=1 \
-    --iodepth=1 \
-    --filename=single_stream_reads_direct/file \
-    --direct=1
-
-  run_fio_test "single_stream_iodepth" \
-    --rw=randread \
-    --bs=8K \
-    --filesize=8M \
-    --numjobs=1 \
-    --nrfiles=1 \
-    --iodepth=8 \
-    --filename=single_stream_iodepth/file
-
-  run_fio_test "multi_job_shared_client" \
-    --rw=randread \
-    --bs=8K \
-    --filesize=8M \
-    --numjobs=4 \
-    --nrfiles=1 \
-    --iodepth=1 \
-    --filename_format='multi_job_shared_client/file.$jobnum' \
-    --go-storage-threads-share-client=1
-
-  run_fio_test "multi_job_unique_clients" \
-    --rw=randread \
-    --bs=8K \
-    --filesize=8M \
-    --numjobs=4 \
-    --nrfiles=1 \
-    --iodepth=1 \
-    --filename_format='multi_job_unique_clients/file.$jobnum' \
-    --go-storage-threads-share-client=0
-
-  run_fio_test "write_buffered" \
-    --rw=write \
-    --bs=4K \
-    --filesize=8M \
-    --numjobs=1 \
-    --iodepth=1 \
-    --filename=write_buffered/file \
-    --direct=0
-
-  run_fio_test "write_direct" \
-    --rw=write \
-    --bs=4K \
-    --filesize=8M \
-    --numjobs=1 \
-    --iodepth=1 \
-    --filename=write_direct/file \
-    --direct=1
-
-  run_fio_test "multi_write" \
-    --rw=write \
-    --bs=4K \
-    --filesize=2M \
-    --numjobs=4 \
-    --iodepth=1 \
-    --filename_format='multi_write/file.$jobnum'
-
-  run_fio_test "single_job_multi_write" \
-    --rw=write \
-    --bs=4K \
-    --filesize=2M \
-    --numjobs=1 \
-    --nrfiles=4 \
-    --iodepth=1 \
-    --filename_format='single_job_multi_write/file.$filenum'
-
-  run_fio_test "large_reads" \
-    --rw=randread \
-    --bs=1M \
-    --filesize=4M \
-    --numjobs=3 \
-    --nrfiles=1 \
-    --iodepth=2 \
-    --filename_format='large_reads/file.$jobnum'
+  run_fio_test single_stream_reads_buffered
+  run_fio_test single_stream_reads_direct
+  run_fio_test single_stream_iodepth
+  run_fio_test multi_job_shared_client
+  run_fio_test multi_job_unique_clients
+  run_fio_test write_buffered
+  run_fio_test write_direct
+  run_fio_test multi_write
+  run_fio_test single_job_multi_write
+  run_fio_test large_reads
 
   echo "All FIO integration tests completed successfully!"
+}
+
+# Callback function called by the testbench helper
+execute_tests() {
+  for append_writes in 0 1; do
+    for finalize_on_close in 0 1; do
+      for range_reader in 0 1; do
+        execute_one_test \
+          "$@" \
+          --go-storage-append-writes="${append_writes?}" \
+          --go-storage-finalize-on-close="${finalize_on_close?}" \
+          --go-storage-range-reader="${range_reader?}"
+      done
+    done
+  done
 }
 
 # Run the testbench and execute our tests
